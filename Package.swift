210cffb69ef04f906fc3f44b7b3329292d1f69c9
António Ramos <aramos@blissapplications.com>
2024-03-28 18:37:24 +0000

    Updated RX and Alamofire, removed tests

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 let rocketIfNeeded: [Package.Dependency]
 
 #if os(OSX) || os(Linux)
-rocketIfNeeded = [
-    .package(url: "https://github.com/shibapm/Rocket", .upToNextMajor(from: "1.2.0")) // dev
-]
+rocketIfNeeded = []
 #else
 rocketIfNeeded = []
 #endif
@@ -27,13 +25,10 @@ let package = Package(
         .library(name: "RxMoya", targets: ["RxMoya"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")), // dev
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")), // dev
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")) // dev
-    ] + rocketIfNeeded,
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.0")),
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "7.1.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.6.0")),
+    ],
     targets: [
         .target(
             name: "Moya",
@@ -63,19 +58,7 @@ let package = Package(
                 "Moya",
                 .product(name: "RxSwift", package: "RxSwift")
             ]
-        ),
-        .testTarget( // dev
-            name: "MoyaTests",  // dev
-            dependencies: [ // dev
-                "Moya", // dev
-                "CombineMoya", // dev
-                "ReactiveMoya", // dev
-                "RxMoya", // dev
-                .product(name: "Quick", package: "Quick"), // dev
-                .product(name: "Nimble", package: "Nimble"), // dev
-                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs") // dev
-            ] // dev
-        ) // dev
+        )
     ]
 )
 
